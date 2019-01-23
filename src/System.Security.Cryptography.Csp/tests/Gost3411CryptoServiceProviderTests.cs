410f1f2caec466efe3ebb98bdb76ba489ef0a88e
Anatoly Belyaev <tolya.msu@gmail.com>
2019-01-23 19:00:47 +0300

    add Gost3411_2012XCryptoServiceProviderTests

--- src/System.Security.Cryptography.Csp/tests/Gost3411CryptoServiceProviderTests.cs
+++ src/System.Security.Cryptography.Csp/tests/Gost3411CryptoServiceProviderTests.cs
@@ -9,55 +9,71 @@ namespace System.Security.Cryptography.Encryption.Gost3411.Tests
 {
     using System.Linq;
 
-    using Gost3411 = System.Security.Cryptography.Gost3411;
-
     /// <summary>
-    /// Since SHAxCryptoServiceProvider types wraps IncrementalHash from Algorithms assembly, we only test minimally here.
+    /// Since Gost3411xCryptoServiceProvider types wraps IncrementalHash from Algorithms assembly, we only test minimally here.
     /// </summary>
     public class Gost3411CryptoServiceProviderTests
     {
-        private static readonly byte[] bytesToHash =
-            new byte[]
-            {
-                0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
-                0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
-            };
-
-        private static readonly byte[] computedHash =
-            new byte[]
-            {
-                0x67, 0x3F, 0x02, 0xB6, 0x97, 0xE0, 0xEC, 0xA2,
-                0xC3, 0xEA, 0xA6, 0x48, 0xB5, 0x62, 0x9A, 0x27,
-                0xC1, 0x5A, 0x79, 0x44, 0xB7, 0xE7, 0x10, 0xED,
-                0x8A, 0x4C, 0xF8, 0xCE, 0xA9, 0x09, 0x9E, 0x0F
-            };
-
-
         [Fact]
-        public void Gost3411CreateAndComputeHash()
+        public void GOST3411_Empty()
         {
-            var gost = Gost3411.Create();
-            var hash = gost.ComputeHash(bytesToHash);
-            var isValidHash = hash.SequenceEqual(computedHash);
-
-            Assert.True(isValidHash);
+            Verify<Gost3411CryptoServiceProvider>(string.Empty, "981E5F3CA30C841487830F84FB433E13AC1101569B9C13584AC483234CD656C0");
         }
 
         [Fact]
-        public void Gost3411ConstructorAndHash()
+        public void GOST3411_2012_256_Empty()
         {
-            Gost3411 gost = new Gost3411CryptoServiceProvider();
-            var hash = gost.ComputeHash(bytesToHash);
-            var isValidHash = hash.SequenceEqual(computedHash);
-
-            Assert.True(isValidHash);
+            Verify<Gost3411_2012_256CryptoServiceProvider>(string.Empty, "3F539A213E97C802CC229D474C6AA32A825A360B2A933A949FD925208D9CE1BB");
         }
 
         [Fact]
-        public void Gost3411ComputeHashTwice()
+        public void GOST3411_2012_512_Empty()
         {
-            Gost3411 gost = Gost3411.Create("GOST3411");
-            gost.ComputeHash(gost.ComputeHash(bytesToHash));
+            Verify<Gost3411_2012_512CryptoServiceProvider>(string.Empty, "8E945DA209AA869F0455928529BCAE4679E9873AB707B55315F56CEB98BEF0A7362F715528356EE83CDA5F2AAC4C6AD2BA3A715C1BCD81CB8E9F90BF4C1C1A8A");
+        }
+
+        [Fact]
+        public void GOST3411()
+        {
+            Verify<Gost3411CryptoServiceProvider>(
+                "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+                "FA0F6806E76264FEF2735DDA4232C9C54DEE4CEC82278B50FF76560AD2C61A3E");
+        }
+
+        [Fact]
+        public void GOST3411_2012_256()
+        {
+            Verify<Gost3411_2012_256CryptoServiceProvider>(
+                "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+                "47440B6CA733F24C7B80DADA8055796A2742CB729F92CB7FEDF5188F5F3F1CFC");
+        }
+
+        [Fact]
+        public void GOST3411_2012_512()
+        {
+            Verify<Gost3411_2012_512CryptoServiceProvider>(
+                "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+                "859190F728250159B34A08B1D3262279A19668C571FC7A7E724C0910318FD4A251974E67592DBC96919D282DE2DA875488D59DC37A2876296F633F451A488E24");
+        }
+
+
+        private void Verify<T>(string rawText, string expected) where T:HashAlgorithm, new()
+        {
+            byte[] inputBytes = ByteUtils.AsciiBytes(rawText);
+            byte[] expectedBytes = ByteUtils.HexToByteArray(expected);
+
+            using (HashAlgorithm hash = new T())
+            {
+                Assert.True(hash.HashSize > 0);
+                byte[] actual = hash.ComputeHash(inputBytes, 0, inputBytes.Length);
+                byte[] actual_second = hash.ComputeHash(inputBytes);
+
+                Assert.Equal(expectedBytes, actual);
+                Assert.Equal(expectedBytes, actual_second);
+
+                actual = hash.Hash;
+                Assert.Equal(expectedBytes, actual);
+            }
         }
     }
 }
