03e10c536c45d88932a16ab5ba2ae4d95717b752
Anatoly Belyaev <tolya.msu@gmail.com>
2019-07-05 12:19:50 +0300

    using SafeBCryptKeyHandle in X509Pal.PublicKey.Win.cs

--- src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.Windows/X509Pal.PublicKey.Win.cs
+++ src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.Windows/X509Pal.PublicKey.Win.cs
@@ -11,7 +11,7 @@ using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 using NTSTATUS = Interop.BCrypt.NTSTATUS;
-using SafeBCryptKeyHandle = System.Security.Cryptography.SafeKeyHandle;
+using SafeBCryptKeyHandle = System.Security.Cryptography.SafeBCryptKeyHandle;
 
 using static Interop.Crypt32;
 
